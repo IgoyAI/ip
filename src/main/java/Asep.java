6db4a95ff67a7ae3657995c17b6469904613882e
IgoyAI <aagdeyogipramana@mail.ugm.ac.id>
2025-01-24 15:49:26 +0800

    Rename Duke.java to Asep.java

---
+++ src/main/java/Asep.java
@@ -0,0 +1,12 @@
+public class Asep {
+    public static void main(String[] args) {
+        // Greeting message
+        System.out.println("-----------------------------------");
+        System.out.println("Hello! I'm Asep");
+        System.out.println("What can I do for you?");
+        System.out.println("-----------------------------------");
+
+        // Exit message
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
