af65764b29dde72d56b56e3db201d3380db0d310
IgoyAI <aagdeyogipramana@mail.ugm.ac.id>
2025-02-05 10:55:11 +0800

    Add user input handling to echo commands and exit on "bye"

--- src/main/java/Asep.java
+++ src/main/java/Asep.java
@@ -1,12 +1,30 @@
+import java.util.Scanner;
+
 public class Asep {
     public static void main(String[] args) {
-        // Greeting message
-        System.out.println("-----------------------------------");
-        System.out.println("Hello! I'm Asep");
-        System.out.println("What can I do for you?");
-        System.out.println("-----------------------------------");
+        Scanner scanner = new Scanner(System.in);
 
-        // Exit message
-        System.out.println("Bye. Hope to see you again soon!");
+        // Greeting message
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm Asep");
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        while (true) {
+            String userInput = scanner.nextLine();
+
+            if (userInput.equalsIgnoreCase("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+
+            System.out.println("____________________________________________________________");
+            System.out.println(" " + userInput);
+            System.out.println("____________________________________________________________");
+        }
+
+        scanner.close();
     }
 }
